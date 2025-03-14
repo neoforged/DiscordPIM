3786f9c6c31381cda8f88bc2439927b9298f055f
Marc Hermans <marc.hermans@ldtteam.com>
2025-03-14 18:18:06 +0000

    Add documentation site and start working on the gradle setup.

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/workspaces/discordpim/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/workspaces/discordpim/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/workspaces/discordpim/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/workspaces/discordpim/docs/src/css/custom.css"),
+];
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,82 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'DiscordPIM',
+  tagline: 'Privileged Identity Management in Discord',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://discordpim.neoforged.net',
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'DiscordPIM - Docs',
+      logo: {
+        alt: 'DiscordPIM',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/neoforged/DiscordPIM',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} NeoForged Project. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
